0420896c8e992d6e6f20d0cbb71375973a5e4ea1
berger <robertberger.dev@gmail.com>
2023-02-20 11:56:54 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContactActions from './ContactActions'
 import ContactForm from './ContactForm'
 import ContactList from './ContactList'
 import Stack from 'react-bootstrap/Stack'
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
+  function handleNewContact(contact) {
+    setContacts(contacts.concat(contact))
+  }
+
   return (
     <Stack gap={3} className="col-md-10 mx-auto">
       <ContactActions selectedContact={null} />
-      <ContactForm />
-      <ContactList contacts={[]} />
+      <ContactForm onNewContact={handleNewContact} />
+      <ContactList contacts={contacts} />
     </Stack>
   )
 }
